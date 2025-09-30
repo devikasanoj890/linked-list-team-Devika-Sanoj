6531c15d37df95b255417d3ed5040782618819a6
devikasanoj890 <devikasanoj2029@mca.ajce.in>
2025-09-30 16:45:26 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,2 +1,70 @@
-My Linked List Repo
-C program for singly linked list
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node* createNode(int data) {
+    struct Node *newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertAtBeg(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = head;
+    return newNode;
+}
+
+struct Node* insertAtEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL) {
+        return newNode;
+    }
+    struct Node *temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return head;
+}
+
+void print(struct Node *head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ->", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct Node* head = createNode(20);
+    struct Node* second = createNode(30);
+    head->next = second;
+    second->next = NULL;
+
+    printf("Linked List : ");
+    print(head);
+
+    head = insertAtBeg(head, 26);
+    head = insertAtEnd(head, 25);
+
+    printf("Linked List after insertion :\n");
+    print(head);
+    freeList(head); 
+    return 0;
+}
+
